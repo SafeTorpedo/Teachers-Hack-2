a3e61b6f7de919bd71bfff1aacfc3272f5934c1d
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-09-24 18:03:32 +0530

    Installed tailwind, added Navbar component

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,20 +5,18 @@ import React from "react";
 const Form = () => {
     const [items, setItems] = useState({});
 
-// useEffect(() => {
-//   localStorage.setItem('items', JSON.stringify(items));
-// }, [items]);
+    // useEffect(() => {
+    //   localStorage.setItem('items', JSON.stringify(items));
+    // }, [items]);
 
-const handle = () => {
-    localStorage.setItem('photo', items);
-   
- };
- const remove = () => {
-    localStorage.removeItem('photo', items);
-   
- };
+    const handle = () => {
+        localStorage.setItem("photo", items);
+    };
+    const remove = () => {
+        localStorage.removeItem("photo", items);
+    };
     return (
-        <div className="mt-[-166px] md:col-span-2 md: text-center py-10">
+        <div className="md:col-span-2 md: text-center py-10">
             <form>
                 <div className="overflow-hidden shadow sm:rounded-md">
                     <div className=" px-4 py-5 sm:p-6">
@@ -36,20 +34,18 @@ const handle = () => {
                                     className=" placeholder:text-gray-600 mt-1 ml-auto p-6 block w-1/2  rounded-md border-red-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                 />
                             </div>
-
-                            
                         </div>
                     </div>
                     <div className=" px-4 py-2 text-center sm:px-6">
                         <button
-                        onClick={handle}
+                            onClick={handle}
                             type="submit"
                             className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
                             Add to localstorage
                         </button>
                         <button
-                        onClick={remove}
+                            onClick={remove}
                             type="submit"
                             className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
