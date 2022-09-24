bd4a20fdbfec54ba9c5a06fdfc631c9c38d059d2
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-09-25 02:29:28 +0530

    Merged Hooks and Canvas components

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import Hero from "./components/Hero";
 import Canvas from "./components/Canvas";
 
 function App() {
-    const reactData = [
-        { id: 1, name: " Tom" },
-        { id: 2, name: " Sarah" },
-    ];
-    const url = "localhost:3000/api/users/register";
-    let sendData = () => {
-        axios
-            .post(url, reactData)
-            .then((res) => console.log("Data send"))
-            .catch((err) => console.log(err.data));
-    };
-    sendData();
     return (
         <div className="App">
             <Navbar />
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,87 @@
-import { useOnDraw } from "./Hooks";
+import { useEffect, useRef } from "react";
 
 const Canvas = ({ width, height }) => {
+    const canvasRef = useRef(null);
+    function useOnDraw(onDraw) {
+        const isDrawingRef = useRef(false);
+        const prevPointRef = useRef(null);
+
+        const mouseMoveListenerRef = useRef(null);
+        const mouseUpListenerRef = useRef(null);
+
+        function setCanvasRef(ref) {
+            canvasRef.current = ref;
+        }
+
+        function onCanvasMouseDown() {
+            isDrawingRef.current = true;
+        }
+
+        useEffect(() => {
+            function computePointInCanvas(clientX, clientY) {
+                if (canvasRef.current) {
+                    const boundingRect =
+                        canvasRef.current.getBoundingClientRect();
+                    return {
+                        x: clientX - boundingRect.left,
+                        y: clientY - boundingRect.top,
+                    };
+                } else {
+                    return null;
+                }
+            }
+            function initMouseMoveListener() {
+                const mouseMoveListener = (e) => {
+                    if (isDrawingRef.current && canvasRef.current) {
+                        const point = computePointInCanvas(
+                            e.clientX,
+                            e.clientY
+                        );
+                        const ctx = canvasRef.current.getContext("2d");
+                        if (onDraw) onDraw(ctx, point, prevPointRef.current);
+                        prevPointRef.current = point;
+                        console.log(point);
+                    }
+                };
+                mouseMoveListenerRef.current = mouseMoveListener;
+                window.addEventListener("mousemove", mouseMoveListener);
+            }
+
+            function initMouseUpListener() {
+                const listener = () => {
+                    isDrawingRef.current = false;
+                    prevPointRef.current = null;
+                };
+                mouseUpListenerRef.current = listener;
+                window.addEventListener("mouseup", listener);
+            }
+
+            function cleanup() {
+                if (mouseMoveListenerRef.current) {
+                    window.removeEventListener(
+                        "mousemove",
+                        mouseMoveListenerRef.current
+                    );
+                }
+                if (mouseUpListenerRef.current) {
+                    window.removeEventListener(
+                        "mouseup",
+                        mouseUpListenerRef.current
+                    );
+                }
+            }
+
+            initMouseMoveListener();
+            initMouseUpListener();
+            return () => cleanup();
+        }, [onDraw]);
+
+        return {
+            setCanvasRef,
+            onCanvasMouseDown,
+        };
+    }
+
     const { setCanvasRef, onCanvasMouseDown } = useOnDraw(onDraw);
 
     function onDraw(ctx, point, prevPoint) {
@@ -22,6 +103,13 @@ const Canvas = ({ width, height }) => {
         ctx.fill();
     }
 
+    const saveImageToLocal = (event) => {
+        let link = event.currentTarget;
+        link.setAttribute("download", "canvas.png");
+        let image = canvasRef.current.toDataURL("image/png");
+        link.setAttribute("href", image);
+    };
+
     return (
         <div className="flex justify-center align-middle">
             <div className="px-6 py-6 flex justify-center text-lg">
@@ -34,6 +122,13 @@ const Canvas = ({ width, height }) => {
                 style={canvasStyle}
                 ref={setCanvasRef}
             />
+            <a
+                id="download_image_link"
+                href="download_link"
+                onClick={saveImageToLocal}
+            >
+                Download
+            </a>
         </div>
     );
 };
