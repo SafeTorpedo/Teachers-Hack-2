3f9636fbd34d176f6bf2aa553625de2e44cf7256
Kaushik-Iyer <kushlaser@gmail.com>
2022-09-24 11:15:40 +0530

    Adding scripts file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios'
 
 function App() {
+  const reactData = [{ id: 1, name: ' Tom' }, { id: 2, name: ' Sarah' }];
+  const url = 'localhost:3000/api/users/register';
+  let sendData = () => {
+    axios.post(url, reactData)
+      .then(res => console.log('Data send'))
+      .catch(err => console.log(err.data))
+  }
+  sendData()
   return (
     <div className="App">
       <header className="App-header">
